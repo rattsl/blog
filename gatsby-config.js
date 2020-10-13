d64220f02dbad03457f1679b5a1ea373b99bf902
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2020-10-13 18:11:42 +0900

    feat: add Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,13 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        head: true,
+      }
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content`,
